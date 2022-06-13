854c85629d342fc931584820a2bbc49fc9763b7a
Lestar <oleh.halushchak@gmail.com>
2022-06-13 18:49:53 +0300

    Find index of a column with the least total value

--- src/Main.java
+++ src/Main.java
@@ -3,39 +3,48 @@
 // знайти номер радка, у якому сума чисел є найбільшою,
 // знайти середнє арифметичне елементів, які знаходяться на діагоналях
 
-import java.util.Arrays;
 import java.util.Random;
-import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        int m = 10;
-        int n = 10;
-        int[][] Matrix = new int[m][m];
+        int m = 3;
+        int n = 4;
+        int[][] Matrix = new int[m][n];
         Random generator = new Random(); // ств. генератор випадкових чисел
         int gn;
-        for (int i = 0; i < m; i++)
+        for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 gn = generator.nextInt(10);
                 Matrix[i][j] = gn;
             }
+        }
+
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++)
+            for (int j = 0; j < n; j++) {
                 System.out.print(Matrix[i][j] + "    ");
+            }
+
             System.out.println();
         }
+
         // знайти номер стовбця у якому сума чисел є найменшою
         int sum = 0;
-      int min=0;
+        int minSumColumnIndex = 0;
 
-        for (int i = 0; i < m; i++) {
-            sum = IntStream.of(Matrix[i]).sum();
-            if (sum < min)
-                min = Arrays.stream(Matrix[i]).sum();
+        for (int i = 0; i < n; i++) {
+            int columnSum = 0;
+            for (int j = 0; j < m; j++) {
+                columnSum += Matrix[j][i];
+            }
 
-//            System.out.print("sum=" + sum);
-//            System.out.println();
+            if (sum > columnSum) {
+                minSumColumnIndex = i;
+            }
         }
-        System.out.println("min=" + min);
+
+        System.out.println("minSumColumnIndex = " + minSumColumnIndex);
+
+        // знайти номер радка, у якому сума чисел є найбільшою
+
     }
 }
