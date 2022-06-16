8c56e0f0eeeece2e86c6b9bb25414f3c21eca5e6
petrotsupryk <petyatsupryk>
2022-06-16 23:23:41 +0300

    // знайти номер радка, у якому сума чисел є найбільшою,

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,61 @@
-// оголосити двовимірний масив 10х10,
 
-// знайти номер радка, у якому сума чисел є найбільшою,
 // знайти середнє арифметичне елементів, які знаходяться на діагоналях
 
-import java.util.Arrays;
 import java.util.Random;
-import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        int m = 10;
-        int n = 10;
-        int[][] Matrix = new int[m][m];
+        int m = 3;
+        int n = 4;
+        int[][] Matrix = new int[m][n];
         Random generator = new Random(); // ств. генератор випадкових чисел
         int gn;
-        for (int i = 0; i < m; i++)
+        for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 gn = generator.nextInt(10);
                 Matrix[i][j] = gn;
             }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++)
-                System.out.print(Matrix[i][j] + "    ");
-            System.out.println();
         }
-        // знайти номер стовбця у якому сума чисел є найменшою
-        int sum = 0;
-        int min = 0;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                sum += Matrix[i][j];
+                System.out.print(Matrix[i][j] + "    ");
             }
-            System.out.println("Suma elementiv " + i + " ryadka " + sum);
+
+            System.out.println();
         }
-        if (sum < min)
-            min = sum;
-        System.out.println("Sum min " + sum);
+
+        // знайти номер стовбця у якому сума чисел є найменшою
+        int sum = 0;
+        int minSumColumnIndex = 0;
+
+        for (int i = 0; i < n; i++) {
+            int columnSum = 0;
+            for (int j = 0; j < m; j++) {
+                columnSum += Matrix[j][i];
+            }
+
+            if (sum > columnSum || minSumColumnIndex == 0) {
+                minSumColumnIndex = i;
+                sum = columnSum;
+            }
+        }
+
+        System.out.println("minSumColumnIndex = " + minSumColumnIndex);
+
+        // знайти номер радка, у якому сума чисел є найбільшою
+        int maxSumColumIndex = 0;
+        for (int i = 0; i < n; i++) {
+            int columSum = 0;
+            for (int j = 0; j < m; j++) {
+                columSum += Matrix[j][i];
+            }
+            if (sum < columSum || maxSumColumIndex == 0) {
+                maxSumColumIndex = i;
+                sum = columSum;
+            }
+        }
+        System.out.println("maxSumColumnIndex = " + maxSumColumIndex);
+
     }
-
 }
-
